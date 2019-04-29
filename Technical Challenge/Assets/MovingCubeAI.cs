6f9fbe59f5f731757d9103a48030905eb72e74e8
David Hudson <davidhudson42@me.com>
2019-04-28 22:45:34 -0700

    Technical Challenge
    Put Technical Challenge in repository

---
+++ Technical Challenge/Assets/MovingCubeAI.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingCubeAI : MonoBehaviour {
+	private GameObject center;
+	private Animator animator;
+	private float distance;
+	private bool movingToward = true;
+	private float speed;
+
+	// Use this for initialization
+	void Start () {
+		center = GameObject.FindWithTag ("Player");
+		animator = gameObject.GetComponent<Animator> ();distance = Vector3.Distance (center.transform.position, transform.position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		speed = (((float)1.25 * (distance - 4))) * Time.deltaTime;
+
+		if (movingToward == true) {
+			transform.position = Vector3.MoveTowards (transform.position, center.transform.position, speed);
+		} else {
+			transform.position = Vector3.MoveTowards (transform.position, 2 * (transform.position - center.transform.position), speed);
+		}
+
+		distance = Vector3.Distance (center.transform.position, transform.position);
+		animator.SetFloat ("distanceFromCenter", distance);
+	}
+
+	public void setMovementToward (bool newValue) {
+		movingToward = newValue;
+	}
+}
---
+++ Technical Challenge/Assets/rotationGameobject.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotationGameobject : MonoBehaviour {
+
+	public float rotationRate= 10f;
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Gameobject's Rotation Started :)");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (Vector3.down, rotationRate * Time.deltaTime);
+		
+	}
+}
